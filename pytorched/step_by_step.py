315d85717ea2578f636cfc0a5f3ce30f26929035
Nenad Bozinovic <nesaboz@gmail.com>
2022-12-08 13:51:46 -0800

    Added multiclass image classification with hooks, filter and output visualizations.

--- pytorched/step_by_step.py
+++ pytorched/step_by_step.py
@@ -40,13 +40,13 @@ class StepByStep(object):
         self.losses = []
         self.val_losses = []
 
-        self.pera = 0
+        self.visualization = {}
+        self.handles = {}
 
     def __str__(self):
         return f"{self.model}, {self.optimizer}, {self.loss_fn}"
 
     __repr__ = __str__
-
 
     def set_loaders(self, train_loader, val_loader):
         self.train_loader = train_loader
@@ -116,7 +116,7 @@ class StepByStep(object):
     def predict(self, x):
         self.model.eval()
         # need to evaluate
-        prediction = self.model(torch.tensor(x).float().to(self.device))
+        prediction = self.model(torch.as_tensor(x).float().to(self.device))
         self.model.train()
         return prediction.detach().cpu().numpy()
 
@@ -199,8 +199,8 @@ class StepByStep(object):
         return results
 
     def set_seed(self, seed=42):
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True  # ensures that CUDA selects the same algorithm each time an application is run
+        torch.backends.cudnn.benchmark = False  # causes cuDNN to deterministically select an algorithm, possibly at the cost of reduced performance.
         torch.manual_seed(seed)
         np.random.seed(seed)
         random.seed(seed)
@@ -220,3 +220,177 @@ class StepByStep(object):
         for layer in self.model.modules():
             if hasattr(layer, 'reset_parameters'):
                 layer.reset_parameters()
+
+    @staticmethod
+    def _visualize_tensors(axs, x, y=None, yhat=None,
+                           layer_name='', title=None):
+        # The number of images is the number of subplots in a row
+        n_images = len(axs)
+        # Gets max and min values for scaling the grayscale
+        minv, maxv = np.min(x[:n_images]), np.max(x[:n_images])
+        # For each image
+        for j, image in enumerate(x[:n_images]):
+            ax = axs[j]
+            # Sets title, labels, and removes ticks
+            if title is not None:
+                ax.set_title(f'{title} #{j}', fontsize=12)
+            shp = np.atleast_2d(image).shape
+            ax.set_ylabel(
+                f'{layer_name}\n{shp[0]}x{shp[1]}',
+                rotation=0, labelpad=40, fontsize=10
+            )
+            xlabel1 = '' if y is None else f'\nLabel: {y[j]}'
+            xlabel2 = '' if yhat is None else f'\nPredicted: {yhat[j]}'
+            xlabel = f'{xlabel1}{xlabel2}'
+            if len(xlabel):
+                ax.set_xlabel(xlabel, fontsize=10)
+            ax.set_xticks([])
+            ax.set_yticks([])
+
+            # Plots weight as an image
+            ax.imshow(
+                np.atleast_2d(image.squeeze()),
+                cmap='gray',
+                vmin=minv,
+                vmax=maxv
+            )
+        return
+
+    def visualize_filters(self, layer_name, **kwargs):
+        try:
+            # Gets the layer object from the model
+            layer = self.model
+            for name in layer_name.split('.'):
+                layer = getattr(layer, name)
+            # We are only looking at filters for 2D convolutions
+            if isinstance(layer, nn.Conv2d):
+                # Takes the weight information
+                weights = layer.weight.data.cpu().numpy()
+                # weights -> (channels_out (filter), channels_in, H, W)
+                n_filters, n_channels, _, _ = weights.shape
+
+                # Builds a figure
+                size = (2 * n_channels + 2, 2 * n_filters)
+                fig, axes = plt.subplots(n_filters, n_channels,
+                                         figsize=size)
+                axes = np.atleast_2d(axes)
+                axes = axes.reshape(n_filters, n_channels)
+                # For each channel_out (filter)
+                for i in range(n_filters):
+                    StepByStep._visualize_tensors(
+                        axes[i, :],
+                        weights[i],
+                        layer_name=f'Filter #{i}',
+                        title='Channel'
+                    )
+
+                for ax in axes.flat:
+                    ax.label_outer()
+
+                fig.tight_layout()
+                return fig
+            elif isinstance(layer, nn.Linear):  # I added this part for fun
+                weights = layer.weight.data.cpu().numpy()
+                fig = plt.figure(figsize=(3, 3))
+                plt.imshow(weights, cmap='gray')
+                plt.grid(False)
+                fig.tight_layout()
+                return fig
+        except AttributeError as e:
+            print(e)
+            return
+
+    def attach_hooks(self, layers_to_hook, hook_fn=None):
+        """
+        For hook_fn = None, we'll fill out self.visualization dictionary with layer output.
+
+        Args:
+            layers_to_hook:
+            hook_fn:
+
+        Returns:
+
+        """
+
+        # Clear any previous values
+        self.visualization = {}
+        # Creates the dictionary to map layer objects to their names
+        modules = list(self.model.named_modules())
+        layer_names = {layer: name for name, layer in modules[1:]}
+
+        if hook_fn is None:
+            # Hook function to be attached to the forward pass
+            def hook_fn(layer, inputs, outputs):
+                # Gets the layer name
+                name = layer_names[layer]
+                # Detaches outputs
+                values = outputs.detach().cpu().numpy()
+                # Since the hook function may be called multiple times
+                # for example, if we make predictions for multiple mini-batches
+                # it concatenates the results
+                if self.visualization[name] is None:
+                    self.visualization[name] = values
+                else:
+                    self.visualization[name] = np.concatenate([self.visualization[name], values])
+
+        for name, layer in modules:
+            # If the layer is in our list
+            if name in layers_to_hook:
+                # Initializes the corresponding key in the dictionary
+                self.visualization[name] = None
+                # Register the forward hook and keep the handle in another dict
+                self.handles[name] = layer.register_forward_hook(hook_fn)
+
+    def remove_hooks(self):
+        # Loops through all hooks and removes them
+        for handle in self.handles.values():
+            handle.remove()
+        # Clear the dict, as all hooks have been removed
+        self.handles = {}
+
+    def visualize_outputs(self, layers, n_images=10, y=None, yhat=None):
+        layers = filter(lambda l: l in self.visualization.keys(), layers)
+        layers = list(layers)
+        shapes = [self.visualization[layer].shape for layer in layers]
+        n_rows = [shape[1] if len(shape) == 4 else 1
+                  for shape in shapes]
+        total_rows = np.sum(n_rows)
+
+        fig, axes = plt.subplots(total_rows, n_images,
+                                 figsize=(1.5 * n_images, 1.5 * total_rows))
+        axes = np.atleast_2d(axes).reshape(total_rows, n_images)
+
+        # Loops through the layers, one layer per row of subplots
+        row = 0
+        for i, layer in enumerate(layers):
+            start_row = row
+            # Takes the produced feature maps for that layer
+            output = self.visualization[layer]
+
+            is_vector = len(output.shape) == 2
+
+            for j in range(n_rows[i]):
+                StepByStep._visualize_tensors(
+                    axes[row, :],
+                    output if is_vector else output[:, j].squeeze(),
+                    y,
+                    yhat,
+                    layer_name=layers[i] \
+                        if is_vector \
+                        else f'{layers[i]}\nfil#{row - start_row}',
+                    title='Image' if (row == 0) else None
+                )
+                row += 1
+
+        for ax in axes.flat:
+            ax.label_outer()
+
+        plt.tight_layout()  # for more space use plt.subplots_adjust(top = 0.99, bottom=0.01, hspace=1.5, wspace=0.4)
+        return fig
+
+    @property
+    def accuracy(self):
+        a = self.loader_apply(self.val_loader, self.correct)
+        correct, total = a.sum(axis=0)
+        accuracy = round(float((correct / total * 100.).cpu().numpy()), 2)
+        return accuracy
